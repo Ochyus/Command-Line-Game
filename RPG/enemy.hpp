e5f1aab5d80dcbb45e407c796be47b991c0426c5
Ochyus <ochyusspeed@gmail.com>
2023-09-14 16:00:13 -0400

    fixed tests (maybe)

--- RPG/enemy.hpp
+++ RPG/enemy.hpp
@@ -6,11 +6,4 @@ class Enemy : public Entity
 {
 public:
     Enemy(std::string name = "Blank", std::string type = "Physical", int level = 1);
-    std::string getName();
-    std::string getType();
-    int getHitPoints();
-    int getAttack();
-    int getMagicPoints();
-    int getDefense();
-    int getLevel();
 };
--- tests/test.cpp
+++ tests/test.cpp
@@ -54,8 +54,8 @@ void Test::testPlayer()
 void Test::testEnemy()
 {
     Enemy myEnemy("Enemy", "Physical", 1);
-    Test::assertTrue(myEnemy.getName() == "Main", "name was set up incorrectly");
-    Test::assertTrue(myEnemy.getType() == "Mana", "type was set up incorrectly");
+    Test::assertTrue(myEnemy.getName() == "Enemy", "name was set up incorrectly");
+    Test::assertTrue(myEnemy.getType() == "Physical", "type was set up incorrectly");
     Test::assertTrue(myEnemy.getHitPoints() == 1, "attack was set up incorrectly");
     Test::assertTrue(myEnemy.getAttack() == 1, "attack was set up incorrectly");
     Test::assertTrue(myEnemy.getMagicPoints() == 1, "type was set up incorrectly");
