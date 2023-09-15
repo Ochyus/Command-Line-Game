aa2980df83a97eb47180a96af867ce69be899839
Ochyus <ochyusspeed@gmail.com>
2023-09-15 13:29:10 -0400

    removed player.cpp and entity.cpp as they were identical

--- RPG/enemy.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include "entity.hpp"
-
-class Enemy : public Entity
-{
-public:
-    Enemy(std::string name = "Blank", std::string type = "Physical", int level = 1);
-};
