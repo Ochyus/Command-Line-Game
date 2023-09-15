a667c4a51e3ed01b819d72e65481eeeb060ac739
Ochyus <brendanroley@icloud.com>
2023-09-14 21:40:24 -0400

    updated entity.cpp

--- RPG/entity.cpp
+++ RPG/entity.cpp
@@ -4,18 +4,25 @@
 
 void Entity::setLevel(int level)
 {
-    int maxValue[4]; // level * 5 - 5;
-    int minValue[4]; // level * 4 - 4;
+    this->level = level;
+    int maxValue[3]; // level * 5 - 5;
+    int minValue[3]; // level * 4 - 4;
     if (type == "All around")
     {
-        srand(time(NULL)); // Seed the time
-        // int finalNum = rand()%(maxValue-minValue+1)+minValue; // Generate the number, assign to variable.
+        maxValue[0] = level * 3^2 - 2^2;
+        minValue[0] = level * 2^2 - 1^2;
+        maxValue[1] = level * 3^2 - 2^2;
+        minValue[1] = level * 2^2 - 1^2;
+        maxValue[2] = level * 3^2 - 2^2;
+        minValue[2] = level * 2^2 - 1^2;
+        maxValue[3] = level * 3^2 - 2^2;
+        minValue[3] = level * 2^2 - 1^2;
     }
-    if (level > 0 && level < 10)
-    {
-
-    }
-    
+    srand(time(NULL)); // Seed the time
+    this->hitPoints = rand()%(maxValue[0]-minValue[0]+1)+minValue[0]; // Generate the number, assign to variable.
+    this->attack = rand()%(maxValue[1]-minValue[1]+1)+minValue[1];
+    this->magicPoints = rand()%(maxValue[2]-minValue[2]+1)+minValue[2];
+    this->defense = rand()%(maxValue[3]-minValue[3]+1)+minValue[3];
 }
 
 std::string Entity::getName()
